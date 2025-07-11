a3e9f3be4963947f26303c0e3171ddbf155a3f38
Krishna123-web-crypto <naveenkumarkommi4@gmail.com>
2025-07-11 19:13:23 +0530

    changed about.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -6,7 +6,6 @@ export default function About() {
       <h1>About MovieBook</h1>
       <p>
         MovieBook is your one-stop platform for booking movie tickets online quickly and securely.
-        We aim to make your movie experience seamless from ticket booking to watching your favorite films.
       </p>
       <p>
         Our platform is user-friendly, responsive, and ensures that your movie plans are just a few clicks away.
